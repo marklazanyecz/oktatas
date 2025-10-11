3e904052612fcf22d838c6031fab9e264b969581
Lazanyecz Márk <h479411@stud.u-szeged.hu>
2025-10-11 09:30:48 +0200

    Updating filetree

--- Modern webfejlesztés/scripts/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('alertGomb').addEventListener('click', function() {
-  alert('Helló! Ez egy alert üzenet a JavaScriptből!');
-});
-
-document.getElementById('valtoztatoGomb').addEventListener('click', function() {
-  const focim = document.getElementById('focim');
-  focim.textContent = 'A címsor megváltozott!';
-  focim.style.color = 'orange';
-});
